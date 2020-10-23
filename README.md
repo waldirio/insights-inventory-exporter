866e95b1c51197d0d2607d89fd3ce796227c70c0
Waldirio M Pinheiro <waldirio@gmail.com>
2020-10-23 08:07:57 -0700

    Code enhanced in order to collect the whole information from cloud.redhat.com

--- README.md
+++ README.md
@@ -21,30 +21,38 @@ Options:
   -d, --debug           Debugging output (debug output enables verbose)
 ~~~
 
-# Modes
-
-The `insights-inventory-exporter.py` script has two methods of printing inventory.
-By default, it connects to cloud.redhat.com's API in order to retrieve a list of
-hosts which are in inventory.
-
-Additionally, it has an **offline** mode, which requires usage of the `-f|--filename`
-option. The offline mode is suitable for debugging. It simply requires the output of
-`curl -u $user:$password https://cloud.redhat.com/api/inventory/v1/hosts`
 
 # Example output
 ~~~
-================================================================================
-ip-172-31-33-244.us-east-2.compute.internal Reported by -> puptoo
-  Facts
-    Namespace - rhsm
-      RH_PROD,[u'250', u'479']
-      SYSPURPOSE_SLA,Premium
-      IS_VIRTUAL,True
-      orgId,1234567
-      CPU_CORES,1
-      MEMORY,1
-      SYNC_TIMESTAMP,2020-04-07T00:44:22.606Z
-      CPU_SOCKETS,1
-      ARCHITECTURE,x86_64
-================================================================================
+$ cat compact_version-waldirio-charter-support.csv 
+id,server,reporter,arch,core_per_sockets,infrastructure.type,number_of_cpus,number_of_socket,satellite_managed,subscription_status,satellite_id,hypervisor
+4e42190e-de97-4f7b-a119-e0efcc40589a,vm001.local.domain,puptoo,x86_64,1,virtual,8,8,False,no subscription status key,,No hypervisor
+4914e435-d49e-4f7f-8fac-73614141824c,vm007.local.domain,puptoo,x86_64,1,virtual,8,8,False,no subscription status key,,No hypervisor
+a99297d7-3bf8-47e4-8547-83ee97f886e1,vm0005.local.domain,yupana,x86_64,1,virtual,8,8,True,Fully entitled,944e253d-09a8-444b-acc0-7bb1203a1b57,No hypervisor
+~~~
+
+Also the file `cloud.redhat.com_output-<account_login>.json` will be created when using the flag `-d`. Note, in this file you will be able to see the `partial` information of your Content Host.
+
+# How to implement this script
+- Clone the project
+~~~
+# git@github.com:sideangleside/insights-inventory-exporter.git
+# cd insights-inventory-exporter
+~~~
+- Create a new virtual environment using python 2.7
+~~~
+# python2.7 -m virtualenv /tmp/.virtualenv/insights-inventory-exporter
+~~~
+- Load it
+~~~
+$ source /tmp/.virtualenv/insights-inventory-exporter/bin/activate
+(insights-inventory-exporter) $
+~~~
+- Install the requirements
+~~~
+(insights-inventory-exporter) $ pip install -r requirements 
+~~~
+- Uset it.
+~~~
+./insights-inventory-exporter.py
 ~~~
--- insights-inventory-exporter.py
+++ insights-inventory-exporter.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-
+"""
 # File: insights-inventory-exporter.py
 # Author: Rich Jerrido <rjerrido@outsidaz.org>
-# Purpose: 
-#          
-#          
-#          
+# Contributor: Waldirio M Pinheiro <waldirio@gmail.com>
+# Purpose:
+#   This script will collect the information from cloud.redhat.com/Inventory and
+#   will put in a nice/simple view the whole information, being easy to create a
+#   pivot table and moving on.
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,148 +16,355 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
+"""
 
-import json
-import requests
 import getpass
-import os
+import json
 import urllib2
 import urllib
-import base64
 import sys
-import ssl
 import csv
 from optparse import OptionParser
+import requests
 
-default_login     = "default"
-default_password  = ""
-default_server = "cloud.redhat.com"
+hypervisor_guests_list = []
+hypervisor_guests_final = []
+final_list = []
+complete_list = []
 
-parser = OptionParser()
-parser.add_option("-l", "--login", dest="login", help="Login user", metavar="LOGIN", default=default_login)
-parser.add_option("-f", "--filename", dest="filename", help="Login user", metavar="FILENAME")
-parser.add_option("-p", "--password", dest="password", help="Password for specified user. Will prompt if omitted", metavar="PASSWORD", default=default_password)
-parser.add_option("-s", "--server", dest="server", help="FQDN of server - omit https://", metavar="SERVER", default=default_server)
-parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="Verbose output")
-parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Debugging output (debug output enables verbose)")
-(options, args) = parser.parse_args()
+stage_lst = []
 
+stage1_lst = ["id",
+              "server",
+              "reporter",
+              "arch",
+              "core_per_sockets",
+              "infrastructure.type",
+              "number_of_cpus",
+              "number_of_socket",
+              "satellite_managed",
+              "subscription_status",
+              "satellite_id",
+              "hypervisor"]
 
-
-class error_colors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-
-# if not (options.login and options.server):
-#     print "Must specify login, server, and orgid options.  See usage:"
-#     parser.print_help()
-#     print "\nExample usage: ./insights-inventory-exporter.py -l admin -s cloud.redhat.com"
-#     sys.exit(1)
-# else:
-#     login = options.login
-#     password = options.password
-#     server = options.server
+complete_list.append(stage1_lst)
+stage1_lst = []
 
 
-if options.filename:
-    FILENAME = options.filename
-    FILEINPUTMODE = True
-elif not (options.login and options.server):
-    print "Must specify login, server, and orgid options.  See usage:"
-    parser.print_help()
-    print "\nExample usage: ./insights-inventory-exporter.py -l admin -s cloud.redhat.com"
-    sys.exit(1)
-else:
-    FILEINPUTMODE = False
+def process_info(login, password, server):
+    """
+    Function responsible for collect and process the main info regarding
+    to the Content Hosts on cloud.redhat.com
+    """
 
-login = options.login
-password = options.password
-server = options.server
+    if options.debug:
+        DEBUG = True
+        VERBOSE = True
+        print "[%sDEBUG%s] LOGIN -> %s " % (error_colors.OKBLUE, error_colors.ENDC, login)
+        print "[%sDEBUG%s] PASSWORD -> %s " % (error_colors.OKBLUE, error_colors.ENDC, password)
+        print "[%sDEBUG%s] SERVER -> %s " % (error_colors.OKBLUE, error_colors.ENDC, server)
+    else:
+        DEBUG = False
+        VERBOSE = False
 
-if not (FILEINPUTMODE or password):
-     password = getpass.getpass("%s's password:" % login)
+    if options.verbose:
+        VERBOSE = True
 
-if options.debug:
-    DEBUG = True
-    VERBOSE = True
-    print "[%sDEBUG%s] LOGIN -> %s " % (error_colors.OKBLUE, error_colors.ENDC, login)
-    print "[%sDEBUG%s] PASSWORD -> %s " % (error_colors.OKBLUE, error_colors.ENDC, password)
-    print "[%sDEBUG%s] SERVER -> %s " % (error_colors.OKBLUE, error_colors.ENDC, server)
-else:
-    DEBUG = False
-    VERBOSE = False
+    if DEBUG and not FILEINPUTMODE:
+        outputfile = open(("cloud.redhat.com_output-%s.json" % login), "w")
 
-if options.verbose:
-    VERBOSE = True
-
-
-
-if DEBUG and not FILEINPUTMODE:
-    outputfile = open(("cloud.redhat.com_output-%s.json" % login), "w")
-
-systemdata = []
-if FILEINPUTMODE:
-    f = open(FILENAME,"r")
-    jsondata = json.load(f)
-    systemdata = jsondata['results']
-else:
-    try:
-        url = 'https://' + server + '/api/inventory/v1/hosts'
-        if VERBOSE:
-            print "=" * 80
-            print "[%sVERBOSE%s] Connecting to -> %s " % (error_colors.OKGREEN, error_colors.ENDC, url)
-        result = requests.get(url, auth=(login, password)).content
-        jsonresult = json.loads(result)
-
-        if VERBOSE:
-            print
-            "=" * 80
-    except urllib2.URLError, e:
-        print
-        "Error: cannot connect to the API: %s" % (e)
-        print
-        "Check your URL & try to login using the same user/pass via the WebUI and check the error!"
-        sys.exit(1)
-    except Exception, e:
-        print
-        "FATAL Error - %s" % (e)
-        sys.exit(2)
-
-    try:
-        page = 0
-        per_page = 100
-        while (page == 0 or int(jsonresult['per_page']) == len(jsonresult['results'])):
-            page += 1
-            q = [('page', page), ('per_page', per_page)]
-            url = "https://cloud.redhat.com/api/inventory/v1/hosts?" + urllib.urlencode(q)
+    systemdata = []
+    if FILEINPUTMODE:
+        f = open(FILENAME, "r")
+        jsondata = json.load(f)
+        # systemdata = jsondata['results']
+        systemdata = jsondata
+    else:
+        try:
+            url = 'https://' + server + '/api/inventory/v1/hosts'
             if VERBOSE:
                 print "=" * 80
                 print "[%sVERBOSE%s] Connecting to -> %s " % (error_colors.OKGREEN, error_colors.ENDC, url)
             result = requests.get(url, auth=(login, password)).content
             jsonresult = json.loads(result)
-            systemdata += jsonresult['results']
+
+            if VERBOSE:
+                print
+                "=" * 80
+        except urllib2.URLError, e:
+            print
+            "Error: cannot connect to the API: %s" % (e)
+            print
+            "Check your URL & try to login using the same user/pass via the WebUI and check the error!"
+            sys.exit(1)
+        except Exception, e:
+            print
+            "FATAL Error - %s" % (e)
+            sys.exit(2)
+
+        try:
+            page = 0
+            per_page = 100
+            # For debugging purposes
+            # per_page = 5
+            while (page == 0 or int(jsonresult['per_page']) == len(jsonresult['results'])):
+            # For debugging purposes
+            # while (page < 2):
+                page += 1
+                q = [('page', page), ('per_page', per_page)]
+                url = "https://cloud.redhat.com/api/inventory/v1/hosts?" + urllib.urlencode(q)
+                if VERBOSE:
+                    print "=" * 80
+                    print "[%sVERBOSE%s] Connecting to -> %s " % (error_colors.OKGREEN, error_colors.ENDC, url)
+                result = requests.get(url, auth=(login, password)).content
+                jsonresult = json.loads(result)
+                systemdata += jsonresult['results']
             if DEBUG:
-                outputfile.write(result)
+                with open('cloud.redhat.com_output-%s.json' % login, 'w') as json_file:
+                    json.dump(systemdata, json_file)
 
-    except Exception, e:
-        print "FATAL Error - %s" % (e)
-        sys.exit(2)
+        except Exception, e:
+            print "FATAL Error - %s" % (e)
+            sys.exit(2)
 
-if DEBUG and not FILEINPUTMODE:
-    outputfile.close()
+    if DEBUG and not FILEINPUTMODE:
+        outputfile.close()
+
+    for system in systemdata:
+        print "%s Reported by -> %s" % (system['display_name'], system['reporter'])
+        display_name = system['display_name']
+        reporter = system['reporter']
+        satellite_id = system['satellite_id']
+
+        system_profile(system['id'], display_name, reporter, satellite_id)
 
 
-for system in systemdata:
-	print "=" * 80
-	print "%s Reported by -> %s" % (system['display_name'], system['reporter'])
-	if system.has_key('facts'):
-		print "  Facts"
-		for entry in system['facts']:
-			print "    Namespace - %s" % entry['namespace']
-			for fact, value in entry['facts'].iteritems():
-				print "      %s,%s" % (fact,value) 
+def system_profile(id, display_name, reporter, satellite_id):
+    """
+    Function responsible for collect the system_profile of each Content Host
+    hosted on cloud.redhat.com
+    """
+    global stage_lst
 
+    url = 'https://' + server + '/api/inventory/v1/hosts/' + id + '/system_profile'
+
+    result = requests.get(url, auth=(login, password)).content
+    jsonresult = json.loads(result)
+
+    stage_lst.append(id)
+    stage_lst.append(display_name)
+    stage_lst.append(reporter)
+
+    try:
+        arch = jsonresult['results'][0]['system_profile']['arch']
+        stage_lst.append(arch)
+    except KeyError:
+        stage_lst.append("no arch key")
+
+    try:
+        cores_per_socket = jsonresult['results'][0]['system_profile']['cores_per_socket']
+        stage_lst.append(cores_per_socket)
+    except KeyError:
+        stage_lst.append("no cores per socket key")
+
+    try:
+        infrastructure_type = jsonresult['results'][0]['system_profile']['infrastructure_type']
+        stage_lst.append(infrastructure_type)
+    except KeyError:
+        stage_lst.append("no infrastructure_type key")
+
+    try:
+        number_of_cpus = jsonresult['results'][0]['system_profile']['number_of_cpus']
+        stage_lst.append(number_of_cpus)
+    except KeyError:
+        stage_lst.append("no number of cpus key")
+
+    try:
+        number_of_sockets = jsonresult['results'][0]['system_profile']['number_of_sockets']
+        stage_lst.append(number_of_sockets)
+    except KeyError:
+        stage_lst.append("no number of sockets key")
+
+    try:
+        satellite_managed = jsonresult['results'][0]['system_profile']['satellite_managed']
+        stage_lst.append(satellite_managed)
+    except KeyError:
+        stage_lst.append("no satellite managed key")
+
+    try:
+        subscription_status = jsonresult['results'][0]['system_profile']['subscription_status']
+        stage_lst.append(subscription_status)
+    except KeyError:
+        stage_lst.append("no subscription status key")
+
+    stage_lst.append(satellite_id)
+
+    final_list.append(stage_lst)
+    stage_lst = []
+
+
+def hypervisor_guests():
+    """
+    Function responsible to identify all the hypervisors "virt-who-" and then
+    compare the full list against the hypervisor list. The main idea is to map
+    which Content Host is running on top of which hypervisor.
+    """
+    global hypervisor_guests_list
+    global complete_list
+
+    # Filtering the CH with virt-who in the name
+    stage_lst = []
+    for ch in final_list:
+        if 'virt-who-' in ch[1]:
+            stage_lst.append(ch[1])
+            stage_lst.append(ch[10])
+            hypervisor_guests_list.append(stage_lst)
+            stage_lst = []
+
+    # Running on the list of virt-who-* servers to retrieve the guest list
+    stage_lst = []
+    for hyper in hypervisor_guests_list:
+
+        url = 'https://' + server + '/api/rhsm-subscriptions/v1/hosts/' + hyper[1] + '/guests?limit=100&offset=0'
+
+        result = requests.get(url, auth=(login, password)).content
+        jsonresult = json.loads(result)
+
+        if jsonresult['data']:
+            for srv in jsonresult['data']:
+                stage_lst.append(srv['display_name'])
+                stage_lst.append(srv['inventory_id'])
+                stage_lst.append(hyper[0])
+                hypervisor_guests_final.append(stage_lst)
+                stage_lst = []
+
+    # Checking the complete inventory against the known ch/hypervisors
+    stage_lst = []
+    for each_ch in final_list:
+        count = 0
+        if (len(hypervisor_guests_final) > 0):
+            for mapped_ch in hypervisor_guests_final:
+                if (each_ch[0] == mapped_ch[1]):
+                    count += 1
+                    stage_lst.append(each_ch[0])
+                    stage_lst.append(each_ch[1])
+                    stage_lst.append(each_ch[2])
+                    stage_lst.append(each_ch[3])
+                    stage_lst.append(each_ch[4])
+                    stage_lst.append(each_ch[5])
+                    stage_lst.append(each_ch[6])
+                    stage_lst.append(each_ch[7])
+                    stage_lst.append(each_ch[8])
+                    stage_lst.append(each_ch[9])
+                    stage_lst.append(each_ch[10])
+                    stage_lst.append(mapped_ch[2])
+
+            if (count == 1):
+                complete_list.append(stage_lst)
+                stage_lst = []
+            elif (count == 0):
+                stage_lst.append(each_ch[0])
+                stage_lst.append(each_ch[1])
+                stage_lst.append(each_ch[2])
+                stage_lst.append(each_ch[3])
+                stage_lst.append(each_ch[4])
+                stage_lst.append(each_ch[5])
+                stage_lst.append(each_ch[6])
+                stage_lst.append(each_ch[7])
+                stage_lst.append(each_ch[8])
+                stage_lst.append(each_ch[9])
+                stage_lst.append(each_ch[10])
+                stage_lst.append("No hypervisor")
+                complete_list.append(stage_lst)
+                stage_lst = []
+
+        else:
+            stage_lst.append(each_ch[0])
+            stage_lst.append(each_ch[1])
+            stage_lst.append(each_ch[2])
+            stage_lst.append(each_ch[3])
+            stage_lst.append(each_ch[4])
+            stage_lst.append(each_ch[5])
+            stage_lst.append(each_ch[6])
+            stage_lst.append(each_ch[7])
+            stage_lst.append(each_ch[8])
+            stage_lst.append(each_ch[9])
+            stage_lst.append(each_ch[10])
+            stage_lst.append("No hypervisor")
+            complete_list.append(stage_lst)
+            stage_lst = []
+
+
+def csv_export():
+    """
+    Function that will generate the final csv file.
+    """
+    with open('compact_version-%s.csv' % login, 'w') as csv_file:
+        csv_writer = csv.writer(csv_file)
+
+        for rows in complete_list:
+            csv_writer.writerow(rows)
+
+        print "The file compact_version-{}.csv was created!".format(login)
+
+
+if __name__ == "__main__":
+
+    default_server = "cloud.redhat.com"
+    default_login = "default"
+    default_password = ""
+
+    parser = OptionParser()
+    # parser.add_option("-l", "--login", dest="login", help="Login user", metavar="LOGIN", default=default_login)
+    parser.add_option("-l", "--login", dest="login", help="Login user", metavar="LOGIN")
+    parser.add_option("-f", "--filename", dest="filename", help="Login user", metavar="FILENAME")
+    parser.add_option("-p", "--password", dest="password", help="Password for specified user. Will prompt if omitted", metavar="PASSWORD", default=default_password)
+    parser.add_option("-s", "--server", dest="server", help="FQDN of server - omit https://", metavar="SERVER", default=default_server)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="Verbose output")
+    parser.add_option("-d", "--debug", dest="debug", action="store_true", help="Debugging output (debug output enables verbose)")
+    (options, args) = parser.parse_args()
+
+    class error_colors:
+        HEADER = '\033[95m'
+        OKBLUE = '\033[94m'
+        OKGREEN = '\033[92m'
+        WARNING = '\033[93m'
+        FAIL = '\033[91m'
+        ENDC = '\033[0m'
+
+    if not (options.login and options.server):
+        print "Must specify login, server, and orgid options.  See usage:"
+        parser.print_help()
+        print "\nExample usage: ./insights-inventory-exporter.py -l admin -s cloud.redhat.com"
+        sys.exit(1)
+    else:
+        login = options.login
+        password = options.password
+        server = options.server
+
+    if options.filename:
+        FILENAME = options.filename
+        FILEINPUTMODE = True
+    elif not (options.login and options.server):
+        print "Must specify login, server, and orgid options.  See usage:"
+        parser.print_help()
+        print "\nExample usage: ./insights-inventory-exporter.py -l admin -s cloud.redhat.com"
+        sys.exit(1)
+    else:
+        FILEINPUTMODE = False
+
+    login = options.login
+    password = options.password
+    server = options.server
+
+    if not (FILEINPUTMODE or password):
+        password = getpass.getpass("%s's password:" % login)
+
+    # To process all the CH information from cloud.redhat.com
+    process_info(login, password, server)
+
+    # Collecting the list of guests running on top of the hypervisors list
+    hypervisor_guests()
+
+    # Exporting the data to the csv file
+    csv_export()
